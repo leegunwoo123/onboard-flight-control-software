cd27ac3768b9a5f8622dfd8a91c53f75dd8cff38
tony4979 <tony4979@naver.com>
2024-12-02 15:11:10 +0900

    edit imu_calibration.h

--- test_code/imu_calibration.h
+++ test_code/imu_calibration.h
@@ -8,11 +8,21 @@ struct IMUCalibrationData {
     float offsetX;
     float offsetY;
     float offsetZ;
+    float offsetroll;
+    float offsetpitch;
+    float offsetyaw;
     float offsetGyroX;
     float offsetGyroY;
     float offsetGyroZ;
-    float stlRoll;
-    float stlPitch;
+    float roll_acc;
+    float pitch_acc;
+    float yaw;
+    float accelX_calibrated;
+    float accelY_calibrated;
+    float accelZ_calibrated;
+    float gyroX_calibrated;
+    float gyroY_calibrated;
+    float gyroZ_calibrated;
 };
 
 IMUCalibrationData calibrateIMU();
